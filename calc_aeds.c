45dfa4cd9aa5d7d9ee1bdebede41f0940405b607
stagyl <38473924+stagyl@users.noreply.github.com>
2018-05-03 19:51:40 -0300

    Add files via upload

---
+++ calc_aeds.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+  void main () {
+
+    int base_antes, base_depois, n_antes, n_depois;
+
+    printf("Digite o número que deseja converter e sua base\n");
+    scanf("%d %d", &n_antes, &base_antes);
+    printf("Digite a base para que deseja converter\n");
+    scanf("%d", &base_depois);
+
+    
+    }
